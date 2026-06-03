fd7889cb77868f24daa33d80470fafe77802fe8d
ib <ib@ibs-MacBook-Pro.local>
2026-06-03 16:30:29 -0400

    add dependencies

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ supabase_url = os.getenv("SUPABASE_URL")
 supabase_key = os.getenv("SUPABASE_KEY")
 
 if not supabase_url or not supabase_key:
-    raise ValueError("Missing Supabase credentials. Check .env file.")
+    raise ValueError("Missing db credentials.")
 
 supabase = create_client(supabase_url, supabase_key)
 
